1fd39ed1a70bb56e28fe8dbe27b183bf73d99305
vominhtridz <trilove151@gmail.com>
2024-01-12 04:43:04 +0700

    v

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import todos from './routers/todo.js'
 import mongoose from 'mongoose'
 const app = express()
+const PORT = 5000
 //models xử lý việc thêm sữa xóa route hay database 
 // view tập hợp các file html
 // controller việc điều hướng các route
@@ -12,7 +13,6 @@ app.use(express.json())
 const uri = "mongodb+srv://admin:oJrKO41exh6vb0OG@cluster0.0hffbtn.mongodb.net/?retryWrites=true&w=majority";
 // router
 app.use('/todos', todos)
-const PORT = process.env.PORT || 5000
 mongoose.connect(uri)
 
 .then(()=>{
